713a5dd3643cfd0834ed968f8463fc23e40e299e
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-08-03 00:34:02 +0900

    todoの入力フォームと未完了todo表示領域を作成

---
+++ script/script.js
@@ -0,0 +1,35 @@
+const app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            todos: [],
+            text: '',
+        };
+    },
+    methods: {
+        inputText(e) {
+            this.text = e.target.value;
+        },
+
+        addTodo() {
+            //textが空なら何もしない
+            if (!this.text) {
+                return;
+            }
+            //todoの作成
+            const text = this.text;
+            const id = Math.ceil(Math.random() * 100);
+            const todo = {
+                id,
+                text,
+                isDone: false,
+            };
+            this.todos.push(todo);
+            this.resetText();
+        },
+
+        resetText() {
+            this.text = '';
+        }
+    }
+});
